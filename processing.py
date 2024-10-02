d157756ffec212fefbeb55640d51f8a64e30bdfc
scootern917 <35200810+scootern917@users.noreply.github.com>
2024-10-02 13:51:42 -0400

    Add files via upload

--- processing.py
+++ processing.py
@@ -2,35 +2,89 @@ import numpy as np
 import pandas as pd
 import os
 import itertools
+import tqdm
 
 def score_deck(deck: str,
                seq1: str,
                seq2: str) -> tuple[int]:
+    '''
+    Given a shuffled deck of cards, a sequence chosen by player1, and a sequence chosen by player two, 
+    return the number of cards/tricks for each variation of Penney's Game.
+    
+    deck: randomnly shuffled deck of 52 cards
+    seq1: the 3-card sequence chosen by player 1 (ex. BBB, RBR)
+    seq2: the 3-card sequence chosen by player 2 (ex. RRR, BRB)
+    '''
+    p1_cards = 0
+    p2_cards = 0
+    pile = 2
+    
+    p1_tricks = 0
+    p2_tricks = 0
+    
+    i = 0
+    while i < len(deck) - 2:
+        pile += 1
+        current_sequence = deck[i:i+3]
+        if current_sequence == seq1:
+            p1_cards += pile
+            pile = 2
+            p1_tricks += 1
+            i += 3
+        elif current_sequence == seq2:
+            p2_cards += pile
+            pile = 2 
+            p2_tricks += 1
+            i += 3
+        else:
+            i += 1
+
     return p1_cards, p2_cards, p1_tricks, p2_tricks
+
 
 def calculate_winner(p1_cards: int,
                      p2_cards: int,
                      p1_tricks: int,
                      p2_tricks: int):
-    cards_winner = 0
-    cards_draw = 0
-    tricks_winner = 0
-    tricks_draw = 0
+        '''Given the number of cards and tricks for each player, calculate who wins for cards and tricks, as well as draws for cards and tricks.
+            If player one wins, the winner is set to 0. If player 2 wins, the winner is set to 1.
+            Also indicates if there was a draw.
 
-    # if p2 wins set winner to 1, otherwise it is 0 (including draws).
-    # if there is a draw, set draw counter to 1
-    if p1_cards < p2_cards:
-        cards_winner = 1
-    elif p1_cards == p2_cards:
-        cards_draw = 1
-    if p1_tricks < p2_tricks:
-        tricks_winner = 1
-    elif p1_cards == p2_cards:
-        tricks_draw = 1
-    return cards_winner, cards_draw, tricks_winner, tricks_draw
+        Arguments:
+            p1_cards (int): number of cards player 1 won
+            p2_cards (int): number of cards player 2 won
+            p1_tricks (int): number of tricks player 1 won
+            p2_tricks (int): number of tricks player 2 won
+        
+        Output:
+            cards_winner (int): specifies who won based on cards
+            cards_draw (int): 1 if a draw occurred, 0 otherwise
+            tricks_winner (int): specifies who won based on tricks
+            tricks_draw (int): 1 if a draw occured, 0 otherwise'''
+        cards_winner = 0
+        cards_draw = 0
+        tricks_winner = 0
+        tricks_draw = 0
+
+        # if p2 wins set winner to 1, otherwise it is 0 (including draws).
+        # if there is a draw, set draw counter to 1
+        if p1_cards < p2_cards:
+            cards_winner = 1
+        elif p1_cards == p2_cards:
+            cards_draw = 1
+        if p1_tricks < p2_tricks:
+            tricks_winner = 1
+        elif p1_tricks == p2_tricks:
+            tricks_draw = 1
+        return cards_winner, cards_draw, tricks_winner, tricks_draw
+
 
 def play_one_deck(deck: str,
                   data: str):
+    '''The function takes the deck string as an input and the file path to the data folder.
+    The function plays out games between two players using all possible combinations of their strategies for a given
+     deck of cards, calculates the outcomes (wins and draws), and saves the results in a specified directory.
+     Saved in .npy files.'''
     sequences = ['000', '001', '010', '011', '100', '101', '110', '111']
     combinations = itertools.product(sequences, repeat=2)
     p2_wins_cards = pd.DataFrame(columns=sequences, index=sequences)
@@ -51,8 +105,11 @@ def play_one_deck(deck: str,
     np.save(f'{data}cards_draw/{deck_name}.npy', draws_cards, allow_pickle = True)
     np.save(f'{data}tricks_draw/{deck_name}.npy', draws_tricks, allow_pickle = True)
 
+
 def sum_games(data: str, average: bool):
-    '''Take all of the arrays in the /data folder, and add them together/divide by number of files to get the average'''
+    '''Take all of the arrays in the specified folder, and add them together/divide by number of files to get the average 
+    if we are looking at win/loss (boolean is True). We don't find the average if we are looking at draws as we just want 
+      count how many ties there are (boolean is False) '''
     files = [file for file in os.listdir(data) if os.path.isfile(os.path.join(data, file))] # iterate through /data directory, only process files
     games_total = None # where the sum of the games is going
     for file in files:
@@ -67,64 +124,3 @@ def sum_games(data: str, average: bool):
         return np.divide(games_total, num_games)
     return games_total # divide each individual element by the number of games played
 
-def iterate (deck: ):
-    pass
-
-    return
-# take in a deck and iterate through all possible combos of 
-
-def analyze_all_combinations(simulations):
-    all_sequences = ['{:03b}'.format(i) for i in range(8)]  # Generate sequences as '000', '001', etc.
-    results_v1 = []
-    results_v2 = []
-
-    total_iterations = len(all_sequences) * (len(all_sequences) - 1)  # Total number of (p1, p2) pairs
-
-    with tqdm(total=total_iterations, desc="Processing Pairs") as pbar:
-        for p1 in all_sequences:
-            for p2 in all_sequences:
-
-                if p1 != p2:
-                    p1_wins_v1, p2_wins_v1 = 0, 0
-                    p1_wins_v2, p2_wins_v2 = 0, 0
-
-                    for game in simulations:
-                        p1_cards_v1, p2_cards_v1 = variation1(game, p1, p2)
-                        if p1_cards_v1 > p2_cards_v1:
-                            p1_wins_v1 += 1
-                        elif p2_cards_v1 > p1_cards_v1:
-                            p2_wins_v1 += 1
-                    
-                        p1_cards_v2, p2_cards_v2 = variation2(game, p1, p2)
-                        if p1_cards_v2 > p2_cards_v2:
-                            p1_wins_v2 += 1
-                        elif p2_cards_v2 > p1_cards_v2:
-                            p2_wins_v2 += 1
-                    
-                    results_v1.append({
-                        'Sequence 1': p1, 
-                        'Sequence 2': p2, 
-                        'Player 1 Wins': p1_wins_v1, 
-                        'Player 2 Wins': p2_wins_v1, 
-                        'Player 1 Win %': round((p1_wins_v1/(p1_wins_v1 + p2_wins_v1))*100, 2)
-                    })
-                    results_v2.append({
-                        'Sequence 1': p1, 
-                        'Sequence 2': p2, 
-                        'Player 1 Wins': p1_wins_v2, 
-                        'Player 2 Wins': p2_wins_v2, 
-                        'Player 1 Win %': round((p1_wins_v2/(p1_wins_v2 + p2_wins_v2))*100, 2) 
-                    })
-
-                    pbar.update(1)
-
-    aggreg_df1 = pd.DataFrame(results_v1)
-    aggreg_df2 = pd.DataFrame(results_v2)
-
-    # Ensure 'Sequence 1' and 'Sequence 2' are treated as strings
-    aggreg_df1['Sequence 1'] = aggreg_df1['Sequence 1'].astype(str)
-    aggreg_df1['Sequence 2'] = aggreg_df1['Sequence 2'].astype(str)
-    aggreg_df2['Sequence 1'] = aggreg_df2['Sequence 1'].astype(str)
-    aggreg_df2['Sequence 2'] = aggreg_df2['Sequence 2'].astype(str)
-
-    return aggreg_df1, aggreg_df2
